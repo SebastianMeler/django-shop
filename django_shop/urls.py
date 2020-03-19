6a0c168edf7ee5636600cd9ec8c9af834f6de726
Sebastian <sebastianissimus@gmail.com>
2020-03-19 13:56:18 +0100

    cart views and template

--- django_shop/urls.py
+++ django_shop/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^cart/', include('cart.urls', namespace='cart')),
     url(r'^', include('shop.urls', namespace='shop')),
 ]
 
